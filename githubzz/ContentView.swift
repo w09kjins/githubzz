db0422fd82d2a7affaae0b525dd3f02c30ca9012
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-09-08 11:57:31 +0800

    Initial Commit

---
+++ githubzz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubzz
+//
+//  Created by rgs on 8/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubzz/githubzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubzzApp.swift
+//  githubzz
+//
+//  Created by rgs on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct githubzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
